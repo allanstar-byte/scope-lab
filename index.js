068f00992f8067304fffe8449326a933ca0efbb6
allanstar-byte <allanmutisya263@gmail.com>
2022-06-12 13:34:05 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    let result = customerName.toUpperCase();
+    return (result)
+}
+upperCaseCustomerName()
+
+function setBestCustomer() {
+    var bestCustomer =
+}
